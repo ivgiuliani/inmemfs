7d40e62db545d2296567864483cb41aebb6b183a
Giuliani Vito Ivan <giuliani.v@gmail.com>
2009-01-25 13:28:25 +0100

    Added explanation next to each error

--- errors.h
+++ errors.h
@@ -1,10 +1,11 @@
 #ifndef _ERRORS_H
 #define _ERRORS_H
 
-#define E_CONSTRAINT_VIOLATED -1
-#define E_FILE_CHILD					-2
-#define E_INVALID_SYNTAX			-3
-#define E_CANNOT_PROCEED			-4
-#define E_OUT_OF_BOUNDS				-5
+#define E_CONSTRAINT_VIOLATED -1	/* constraint violated */
+#define E_FILE_CHILD					-2	/* can't add a child to a FILE node */
+#define E_INVALID_SYNTAX			-3	/* command has invalid syntax */
+#define E_CANNOT_PROCEED			-4	/* hit resource limits and can't continue */
+#define E_OUT_OF_BOUNDS				-5	/* invalid range */
+#define E_NO_ROOT							-6	/* no root node selected */
 
 #endif /* _ERRORS_H */
