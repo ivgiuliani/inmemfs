f2a30d12eda2c89b84ca70e4c256fce0b0bc9907
Giuliani Vito Ivan <giuliani.v@gmail.com>
2009-01-26 16:50:32 +0100

    Added test cases for node lists

--- tests/check_tree.c
+++ tests/check_tree.c
@@ -38,7 +38,51 @@ START_TEST (test_node_child_addition)
 	fail_if (node_add_child(children1, children2) != E_FILE_CHILD,
 			"Added a file node to a directory node");
 
+	fail_unless (node_get_children_no(father) == 1,
+			"Children number returned doesn't match real children number");
+
 	node_delete(father);
+}
+END_TEST
+
+START_TEST (test_node_list_creation)
+{
+	struct node_list *nl = NULL;
+
+	nl = node_list_create();
+
+	fail_if (nl == NULL,
+			"Node list hasn't been created");
+}
+END_TEST
+
+START_TEST (test_node_list_add_siblings)
+{
+	struct node_list *nl, *tmpnl;
+	struct node *n1, *n2, *n3;
+	int i = 0;
+
+	nl = node_list_create();
+
+	n1 = node_create("Node 1", N_FILE);
+	n2 = node_create("Node 2", N_FILE);
+	n3 = node_create("Node 3", N_FILE);
+
+	fail_unless (node_list_add_sibling(nl, n1) == n1,
+			"Returned object mismatch from expected object");
+	fail_unless (node_list_add_sibling(nl, n2) == n2,
+			"Returned object mismatch from expected object");
+	fail_unless (node_list_add_sibling(nl, n3) == n3,
+			"Returned object mismatch from expected object");
+
+	tmpnl = nl;
+	while (tmpnl->next != NULL) {
+		tmpnl = tmpnl->next;
+		i++;
+	}
+
+	fail_unless(i == 3,
+			"Unexpected number of siblings");
 }
 END_TEST
 
@@ -50,6 +94,8 @@ inmemfs_suite(void) {
 	suite_add_tcase(s, tc_core);
 	tcase_add_test(tc_core, test_node_creation);
 	tcase_add_test(tc_core, test_node_child_addition);
+	tcase_add_test(tc_core, test_node_list_creation);
+	tcase_add_test(tc_core, test_node_list_add_siblings);
 
 	return s;
 }
