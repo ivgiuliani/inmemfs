2400c557d8d0fbf81d4c76222a317965a0657c34
Giuliani Vito Ivan <giuliani.v@gmail.com>
2009-01-26 20:40:48 +0100

    Test shell behaviour

--- tests/check.c
+++ tests/check.c
@@ -1,7 +1,9 @@
 #include <check.h>
+#include <stdio.h>
 #include <stdlib.h>
 
 #include "../src/errors.h"
+#include "../src/shell.h"
 #include "../src/node.h"
 
 START_TEST (test_node_creation)
@@ -86,6 +88,54 @@ START_TEST (test_node_list_add_siblings)
 }
 END_TEST
 
+START_TEST (test_shell_invalid_command)
+{
+	int ret = shell_parse_line("Invalid command");
+	fail_unless (ret == E_CMD_NOT_FOUND,
+			"Invalid command produces unexpected output");
+}
+END_TEST
+
+START_TEST (test_shell_invalid_syntax)
+{
+	int ret;
+
+	ret = shell_parse_line("createroot");
+	fail_unless (ret == E_INVALID_SYNTAX,
+			"Invalid syntax is not recognized on createroot");
+
+	ret = shell_parse_line("setroot a");
+	fail_unless (ret == E_INVALID_SYNTAX,
+			"Invalid syntax is not recognized on setroot");
+}
+END_TEST
+
+START_TEST (test_shell_root_limits)
+{
+	int i, ret;
+
+	for(i = 0; i < MAX_ROOT_NODES; i++)
+		shell_parse_line("createroot xxx");
+
+	ret = shell_parse_line("createroot over_limit");
+
+	/* revert changes back */
+	for(i = 0; i < MAX_ROOT_NODES; i++)
+		shell_parse_line("deleteroot 1");
+
+	fail_unless(ret == E_CANNOT_PROCEED,
+			"Resource limits are not hitted");
+}
+END_TEST
+
+START_TEST (test_shell_no_root_selected)
+{
+	int ret = shell_parse_line("mkdir testdir");
+	fail_unless(ret == E_NO_ROOT,
+			"A root node is found even if none is selected");
+}
+END_TEST
+
 Suite *
 inmemfs_suite(void) {
 	Suite *s = suite_create("Master");
@@ -96,6 +146,11 @@ inmemfs_suite(void) {
 	tcase_add_test(tc_core, test_node_child_addition);
 	tcase_add_test(tc_core, test_node_list_creation);
 	tcase_add_test(tc_core, test_node_list_add_siblings);
+
+	tcase_add_test(tc_core, test_shell_invalid_command);
+	tcase_add_test(tc_core, test_shell_invalid_syntax);
+	tcase_add_test(tc_core, test_shell_root_limits);
+	tcase_add_test(tc_core, test_shell_no_root_selected);
 
 	return s;
 }
