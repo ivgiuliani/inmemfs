858b10cf0699151b1e4c71e451340d3206975c6e
Giuliani Vito Ivan <giuliani.v@gmail.com>
2009-02-06 18:43:08 +0100

    Forgot to remove reference to removed function

--- src/shell.h
+++ src/shell.h
@@ -29,6 +29,5 @@ int cmd_set_root(char *);
 /* private functions */
 struct node_list *_node_from_num(unsigned int);
 void _set_current_root(struct node_list *);
-unsigned int _get_current_root(void);
 
 #endif /* _SHELL_H */
