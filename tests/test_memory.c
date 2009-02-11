762ee1b80777ce1715443fc398c6703b667eaf0c
Giuliani Vito Ivan <giuliani.v@gmail.com>
2009-02-11 18:50:02 +0100

    Added basic tests on read/write on kfiles

--- tests/test_memory.c
+++ tests/test_memory.c
@@ -1,7 +1,16 @@
+/*
+ * Include tests on memory allocation and on the I/O system
+ */
+
 #include <check.h>
+#include <string.h>
+#include <stdlib.h>
 
 #include "../src/common.h"
 #include "../src/kalloc.h"
+#include "../src/node.h"
+#include "../src/io.h"
+#include "../src/errors.h"
 
 START_TEST (mem_alloc_1chunk)
 {
@@ -49,6 +58,47 @@ START_TEST (mem_alloc_less_than_1_chunk)
 }
 END_TEST
 
+START_TEST (mem_write_node)
+{
+	struct node *root = node_create("root", N_DIRECTORY);
+	struct node *node = node_create("node", N_FILE);
+	KFILE kfile;
+	char *data = "I am writing something on a node";
+	char *buffer = (char *)malloc(strlen(data) + 1);
+
+	node_add_child(root, node);
+	kfile = kopen(root, "node");
+
+	kwrite(kfile, data, strlen(data) + 1);
+	kread(kfile, strlen(data) + 1, buffer);
+
+	fail_unless (strcmp(buffer, data) == 0);
+
+	free(buffer);
+	kclose(kfile);
+}
+END_TEST
+
+START_TEST (mem_cant_write_directory)
+{
+	/* Ensure we can't write anything over a N_DIRECTORY node */
+	struct node *root = node_create("root", N_DIRECTORY);
+	struct node *node = node_create("node", N_DIRECTORY);
+	KFILE kfile;
+	char *data = "I can't write anything here!";
+	char *buffer = (char *)malloc(strlen(data));
+
+	node_add_child(root, node);
+	kfile = kopen(root, "node");
+
+	kwrite(kfile, data, strlen(data));
+	fail_unless (kread(kfile, strlen(data), buffer) == E_INVALID_TYPE);
+
+	free(buffer);
+	kclose(kfile);
+}
+END_TEST
+
 TCase *
 tcase_memory(void) {
 	TCase *tc_memory = tcase_create("Memory allocation tests");
@@ -56,6 +106,8 @@ tcase_memory(void) {
 	tcase_add_test(tc_memory, mem_alloc_1chunk);
 	tcase_add_test(tc_memory, mem_alloc_n_chunks);
 	tcase_add_test(tc_memory, mem_alloc_less_than_1_chunk);
+	tcase_add_test(tc_memory, mem_write_node);
+	tcase_add_test(tc_memory, mem_cant_write_directory);
 
 	return tc_memory;
 }
